783c8bdc93a1c143a9a086f067a65b0fc6675fe0
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-01-28 11:24:17 -0600

    initial commit

---
+++ lib/json_pointer.ex
@@ -0,0 +1,2 @@
+defmodule JsonPointer do
+end
---
+++ test/json_pointer_test.exs
@@ -0,0 +1,8 @@
+defmodule JsonPointerTest do
+  use ExUnit.Case
+  doctest JsonPointer
+
+  test "greets the world" do
+    assert JsonPointer.hello() == :world
+  end
+end
